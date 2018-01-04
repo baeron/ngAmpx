ce71d9d5ab83c3f1e3b1c1153f029f26ae5a750c
Baranoff Ievgen <baranoffei@gmail.com>
2018-01-04 17:26:51 +0200

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/ngampx',
+  //database: 'mongodb://localhost:27017/ngampx',
+  database: 'mongodb://baeron:baeron314@ds241737.mlab.com:41737/ampx',
   secret: 'mysecret'
 }
